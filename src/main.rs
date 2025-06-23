e8b425ea0e8c07585d49de92a839d8cb0365d0b5
Vishesh <visheshdwivedi225544@gmail.com>
2025-06-23 20:26:19 +0000

    feat:Ownership Rules and return Solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
+
+    let name = String :: from( "Vishesh");
     println!("Hello, world!");
+    let (name,len)= get_length(name);
+    println!("length of the String id : {}", len);
+    println!("The name is : {}", name);
+    
+}
+
+fn get_length(name1:String)-> (String,usize) {
+    let len= name1.len();
+     return (name1,len);
 }
